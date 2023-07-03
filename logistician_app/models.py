c35e3b5080ac1b6d76c54717c9cd333f825d0cc8
szmisztal <sz.misztal@gmail.com>
2023-07-03 18:09:40 +0200

    change relations between models

--- logistician_app/models.py
+++ logistician_app/models.py
@@ -25,10 +25,10 @@ class LoadPlace(models.Model):
         return f"Load place: {self.country} - {self.state} - {self.town} {self.postal_code}, st. {self.street} {self.street_number}, Contact: {self.contact_number}"
 
 class TransportationOrder(models.Model):
-    date = models.DateField(default = datetime.date.today)
+    date = models.DateField(default = datetime.date.today, validators = [MinValueValidator(datetime.date.today)])
     trailer_type = models.CharField(max_length = 64, choices = TrailerType.choices)
     load_weight = models.PositiveSmallIntegerField(validators = [MinValueValidator(1), MaxValueValidator(24)])
-    load_place = models.OneToOneField(LoadPlace, on_delete = models.PROTECT)
+    load_place = models.ForeignKey(LoadPlace, on_delete = models.PROTECT, related_name = "transportation_order")
 
 class Delivery(models.Model):
     country = models.CharField(max_length = 64)
@@ -39,7 +39,10 @@ class Delivery(models.Model):
     street_number = models.PositiveSmallIntegerField()
     contact_number = models.PositiveSmallIntegerField(blank = True, null = True)
     cargo_weight = models.PositiveSmallIntegerField(validators = [MinValueValidator(1), MaxValueValidator(24)], default = 'Whole cargo')
-    transportation_order = models.ForeignKey(TransportationOrder, on_delete = models.PROTECT, related_name = 'delivery')
+    transportation_order = models.ForeignKey(TransportationOrder, on_delete = models.PROTECT, related_name = "delivery")
 
     def __str__(self):
-        return f"Delivery place: {self.country} - {self.state} - {self.town} {self.postal_code}, st. {self.street} {self.street_number}, Contact: {self.contact_number}, Cargo: {self.cargo_weight}t."
+        return f"Delivery place: {self.country} - {self.state} - {self.town} {self.postal_code}, st. {self.street} {self.street_number}, Contact: {self.contact_number}, Cargo weight: {self.cargo_weight}t."
+
+
+
