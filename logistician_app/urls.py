71e832c5c97bfb3f224f4e645bdaa2d8f1a44f08
szmisztal <sz.misztal@gmail.com>
2023-07-03 18:08:22 +0200

    fix urls, delete basenames

--- logistician_app/urls.py
+++ logistician_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
-from . import views
+from logistician_app import views
 
 router = routers.DefaultRouter()
-router.register(r'transportation_orders', views.TransportationOrderViewSet, basename = 'transportation_order')
-router.register(r'load_places', views.LoadPlaceViewSet, basename = 'load_place')
-router.register(r'delivery', views.DeliveryViewSet, basename = 'delivery')
+router.register(r'transportation_orders', views.TransportationOrderViewSet)
+router.register(r'load_places', views.LoadPlaceViewSet)
+router.register(r'deliveries', views.DeliveryViewSet)
 
 url_patterns = [
     path('', include(router.urls))
