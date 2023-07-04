0153c8b61937e074262dac77b5c550e2b5e78f12
szmisztal <sz.misztal@gmail.com>
2023-07-04 14:34:40 +0200

    add serializer for TankTrailer objects

--- logistician_app/serializers.py
+++ logistician_app/serializers.py
@@ -1,27 +1,33 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import TransportationOrder, LoadPlace, Delivery
+from .models import TransportationOrder, LoadOrDeliveryPlace, TankerTrailer
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password']
 
-class LoadPlaceSerializer(serializers.HyperlinkedModelSerializer):
+class LoadOrDeliveryPlaceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = LoadPlace
-        fields = ['country', 'state', 'town', 'postal_code', 'street', 'street_number', 'contact_number']
+        model = LoadOrDeliveryPlace
+        fields = ['id', 'country', 'state', 'town', 'postal_code', 'street', 'street_number', 'contact_number']
 
-class DeliverySerializer(serializers.HyperlinkedModelSerializer):
+class TankerTrailerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = Delivery
-        fields = ['country', 'state', 'town', 'postal_code', 'street', 'street_number', 'contact_number', 'cargo_weight']
+        model = TankerTrailer
+        fields = ['chamber_1', 'chamber_2', 'chamber_3', 'chamber_4', 'chamber_5']
 
 class TransportationOrderSerializer(serializers.HyperlinkedModelSerializer):
-    load_place = LoadPlaceSerializer(many = False)
-    delivery = DeliverySerializer(many = True)
+    load_place = LoadOrDeliveryPlaceSerializer(many = False)
+    delivery_place = LoadOrDeliveryPlaceSerializer(many = False)
+    tanker_volume = TankerTrailerSerializer(many = False)
+
     class Meta:
         model = TransportationOrder
-        fields = ['date', 'trailer_type', 'load_place', 'load_weight', 'delivery']
+        fields = ['id', 'date', 'trailer_type', 'load_place', 'tanker_volume', 'load_weight', 'delivery_place']
+
+
+
+
 
 
