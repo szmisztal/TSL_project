13d2a1c4d2204ee8610bbabc6ae7adc94d23cc06
szmisztal <sz.misztal@gmail.com>
2023-07-04 14:35:28 +0200

    add view do TankTrailer model

--- logistician_app/views.py
+++ logistician_app/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import TransportationOrder, LoadPlace, Delivery
-from .serializers import TransportationOrderSerializer, LoadPlaceSerializer, DeliverySerializer
+from .models import TransportationOrder, LoadOrDeliveryPlace, TankerTrailer
+from .serializers import TransportationOrderSerializer, LoadOrDeliveryPlaceSerializer, TankerTrailerSerializer
 
 class TransportationOrderViewSet(viewsets.ModelViewSet):
     queryset = TransportationOrder.objects.all()
     serializer_class = TransportationOrderSerializer
 
-class LoadPlaceViewSet(viewsets.ModelViewSet):
-    queryset = LoadPlace.objects.all()
-    serializer_class = LoadPlaceSerializer
+class LoadOrDeliveryPlaceViewSet(viewsets.ModelViewSet):
+    queryset = LoadOrDeliveryPlace.objects.all()
+    serializer_class = LoadOrDeliveryPlaceSerializer
 
-class DeliveryViewSet(viewsets.ModelViewSet):
-    queryset = Delivery.objects.all()
-    serializer_class = DeliverySerializer
+class TankerTrailerViewSet(viewsets.ModelViewSet):
+    queryset = TankerTrailer.objects.all()
+    serializer_class = TankerTrailerSerializer
