071684b33688baf1f767412a0d7e2af84c157a56
szmisztal <sz.misztal@gmail.com>
2023-07-05 17:28:59 +0200

    add urls for user_app

--- TSL_project/urls.py
+++ TSL_project/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from logistician_app import urls as logistician_urls
+from user_app import urls as user_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('logistician/', include(logistician_urls.urlpatterns))
+    path('logistician/', include(logistician_urls.urlpatterns)),
+    path('accounts/', include(user_urls.urlpatterns))
 ]
