12a4ee26268d1659ef47248129ac12d8db4eab4d
szmisztal <sz.misztal@gmail.com>
2023-07-07 13:56:00 +0200

    add template dir

--- TSL_project/settings.py
+++ TSL_project/settings.py
@@ -41,7 +41,7 @@ ROOT_URLCONF = 'TSL_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
