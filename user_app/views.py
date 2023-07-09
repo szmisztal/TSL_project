41f5d18605aed8a9f67e6fcf4d1506482d5e91b6
szmisztal <sz.misztal@gmail.com>
2023-07-09 12:15:14 +0200

    delete homepage view

--- user_app/views.py
+++ user_app/views.py
@@ -1,12 +1,8 @@
 from rest_framework import viewsets
-from django.views import generic
 from .serializers import UserSerializer
 from .models import CustomUser
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all().order_by("id")
     serializer_class = UserSerializer
-
-class HomepageView(generic.TemplateView):
-    template_name = "index.html"
 
