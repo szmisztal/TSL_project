088eb878bfaa9802f221482282249696e4c2c7c3
szmisztal <sz.misztal@gmail.com>
2023-07-11 13:39:48 +0200

    add urls for CRUD operations for transpororders objects

--- logistician_app/urls.py
+++ logistician_app/urls.py
@@ -4,6 +4,10 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name = 'index.html')),
-    path('all_orders/', views.TransportationOrderViewSet.as_view(), name = 'orders-list'),
-    path('order/<int:pk>', views.TransportationOrderViewSet.as_view(), name = 'transportation_order-detail'),
+
+    path('orders_list/', views.TransportationOrderListView.as_view(), name = 'orders-list'),
+    path('order/<int:pk>/', views.TransportationOrderRetrieveView.as_view(), name = 'order-retrieve'),
+    path('order_create/', views.TransportationOrderCreateView.as_view(), name = 'order-create'),
+    path('order_update/<int:pk>/', views.TransportationOrderUpdateView.as_view(), name = 'order-update'),
+    path('order_destroy/<int:pk>/', views.TransportationOrderDestroyView.as_view(), name = 'order-destroy')
 ]
