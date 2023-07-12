248d9da001b2a022e65b1041bc4c9d5dac745267
szmisztal <sz.misztal@gmail.com>
2023-07-12 11:49:33 +0200

    add PlaceForm

--- logistician_app/forms.py
+++ logistician_app/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import TransportationOrder
+from .models import TransportationOrder, LoadOrDeliveryPlace
 
 class OrderForm(forms.ModelForm):
     class Meta:
         model = TransportationOrder
         fields = ['date', 'trailer_type', 'load_place', 'tanker_volume', 'load_weight', 'delivery_place']
+
+class PlaceForm(forms.ModelForm):
+    class Meta:
+        model = LoadOrDeliveryPlace
+        fields = ['company', 'country', 'state', 'town', 'postal_code', 'street', 'street_number', 'contact_number']
