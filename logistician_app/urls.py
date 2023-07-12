0188288691c984f0277d57511bd5f314a3b3aa6f
szmisztal <sz.misztal@gmail.com>
2023-07-12 11:51:22 +0200

    add urls for LoadOrDelivery place model use

--- logistician_app/urls.py
+++ logistician_app/urls.py
@@ -3,11 +3,22 @@ from logistician_app import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name = 'index.html')),
+    path('', TemplateView.as_view(template_name = 'index.html'), name = 'homepage'),
 
     path('orders_list/', views.TransportationOrderListView.as_view(), name = 'orders-list'),
     path('order/<int:pk>/', views.TransportationOrderRetrieveView.as_view(), name = 'order-retrieve'),
     path('order_create/', views.TransportationOrderCreateView.as_view(), name = 'order-create'),
     path('order_update/<int:pk>/', views.TransportationOrderUpdateView.as_view(), name = 'order-update'),
-    path('order_destroy/<int:pk>/', views.TransportationOrderDestroyView.as_view(), name = 'order-destroy')
+    path('order_destroy/<int:pk>/', views.TransportationOrderDestroyView.as_view(), name = 'order-destroy'),
+
+    path('places_list/', views.PlaceListView.as_view(), name ='places-list'),
+    path('place/<int:pk>/', views.PlaceRetrieveView.as_view(), name = 'place-retrieve'),
+    path('place_create/', views.PlaceCreateView.as_view(), name = 'place-create'),
+    path('place_update/<int:pk>/', views.PlaceUpdateView.as_view(), name = 'place-update'),
+    path('place_destroy/<int:pk>/', views.PlaceDestroyView.as_view(), name = 'place-destroy')
 ]
+
+
+
+
+
