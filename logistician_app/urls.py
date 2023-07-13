63957e9e2ab5432a8cd171662e266e7ff3bc03ae
szmisztal <sz.misztal@gmail.com>
2023-07-13 11:26:34 +0200

    add urls for tanker trailer objects

--- logistician_app/urls.py
+++ logistician_app/urls.py
@@ -11,11 +11,17 @@ urlpatterns = [
     path('order_update/<int:pk>/', views.TransportationOrderUpdateView.as_view(), name = 'order-update'),
     path('order_destroy/<int:pk>/', views.TransportationOrderDestroyView.as_view(), name = 'order-destroy'),
 
-    path('places_list/', views.PlaceListView.as_view(), name ='places-list'),
+    path('places_list/', views.PlaceListView.as_view(), name = 'places-list'),
     path('place/<int:pk>/', views.PlaceRetrieveView.as_view(), name = 'place-retrieve'),
     path('place_create/', views.PlaceCreateView.as_view(), name = 'place-create'),
     path('place_update/<int:pk>/', views.PlaceUpdateView.as_view(), name = 'place-update'),
-    path('place_destroy/<int:pk>/', views.PlaceDestroyView.as_view(), name = 'place-destroy')
+    path('place_destroy/<int:pk>/', views.PlaceDestroyView.as_view(), name = 'place-destroy'),
+
+    path('tankers_list/', views.TankerListView.as_view(), name = 'tankers-list'),
+    path('tanker/<int:pk>/', views.TankerRetrieveView.as_view(), name = 'tanker-retrieve'),
+    path('tanker_create/', views.TankerCreateView.as_view(), name = 'tanker-create'),
+    path('tanker_update/<int:pk>/', views.TankerUpdateView.as_view(), name = 'tanker-update'),
+    path('tanker_destroy/<int:pk>/', views.TankerDestroyView.as_view(), name = 'tanker-destroy')
 ]
 
 
