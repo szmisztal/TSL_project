41b12a1f8fea8ab5b0085a5cb4093bad23d5cfe4
szmisztal <sz.misztal@gmail.com>
2023-07-14 16:12:55 +0200

    add signal to automaticly add Token for new users

--- user_app/models.py
+++ user_app/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, Group
+from rest_framework.authtoken.models import Token
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class UserRole(models.TextChoices):
     LOGISTICIAN = "Logistician"
@@ -10,7 +13,7 @@ class CustomUser(AbstractUser):
     username = models.CharField(max_length = 32, unique = True)
     first_name = models.CharField(max_length = 16)
     last_name = models.CharField(max_length = 16)
-    email = models.EmailField(blank = True, null = True)
+    email = models.EmailField(blank = True, null = True, unique = True)
     phone_number = models.PositiveSmallIntegerField(unique = True)
     role = models.CharField(max_length = 16, choices = UserRole.choices)
 
@@ -29,6 +32,11 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} - {self.role}, phone: {self.phone_number}, mail: {self.email}"
+
+@receiver(post_save, sender = CustomUser)
+def create_auth_token(sender, instance = None, created = False, **kwargs):
+    if created:
+        Token.objects.create(user = instance)
 
 class LogisticianGroup(Group):
     name = "Logisticians group"
