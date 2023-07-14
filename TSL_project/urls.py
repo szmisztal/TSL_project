648323344e2408a8024866a1bd6918aea7b37996
szmisztal <sz.misztal@gmail.com>
2023-07-14 16:11:01 +0200

    change places for url routes

--- TSL_project/urls.py
+++ TSL_project/urls.py
@@ -4,7 +4,7 @@ from logistician_app import urls as logistician_urls
 from user_app import urls as user_urls
 
 urlpatterns = [
+    path('users/', include(user_urls.urlpatterns)),
     path('admin/', admin.site.urls),
     path('logistician/', include(logistician_urls.urlpatterns)),
-    path('users/', include(user_urls.urlpatterns)),
 ]
