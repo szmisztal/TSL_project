fdf032d3242aecde656d5ed0e819b695b9e78cca
szmisztal <sz.misztal@gmail.com>
2023-07-14 16:13:31 +0200

    add 'role' filed to serializer

--- user_app/serializers.py
+++ user_app/serializers.py
@@ -1,14 +1,6 @@
-from rest_framework.authtoken.models import Token
-from django.db.models.signals import post_save
 from django.contrib.auth.hashers import make_password
-from django.dispatch import receiver
 from rest_framework import serializers
 from .models import CustomUser
-
-@receiver(post_save, sender = CustomUser)
-def create_auth_token(sender, instance = None, created = False, **kwargs):
-    if created:
-        Token.objects.create(user = instance)
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
