d05add349a122d3b88e9a4b0aee0ac72c2d83454
szmisztal <sz.misztal@gmail.com>
2023-07-14 16:14:07 +0200

    add urls for login/logout

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from user_app import views
+from .views import sign_in, sign_out
 
 urlpatterns = [
+    path('login/', sign_in, name = 'login'),
     path('', TemplateView.as_view(template_name = 'index.html'), name = 'homepage'),
-    # path('register/')
+    path('logout/', sign_out, name = 'logout')
 ]
