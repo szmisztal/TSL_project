91223beb2dd5323539fdd698aa361c1b25b5a5d8
szmisztal <sz.misztal@gmail.com>
2023-07-17 15:24:52 +0200

    add user registration form

--- user_app/forms.py
+++ user_app/forms.py
@@ -1,5 +1,13 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length = 64)
     password = forms.CharField(max_length = 64, widget = forms.PasswordInput)
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ['username','password1', 'password2', 'first_name', 'last_name', 'email', 'phone_number', 'role']
