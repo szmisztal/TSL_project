9832be75a148bef8ee4122b558c264f7eb8fff9a
szmisztal <sz.misztal@gmail.com>
2023-07-17 15:23:55 +0200

    add url for user registration

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import sign_in, sign_out
+from .views import sign_in, sign_out, sign_up
 
 urlpatterns = [
-    path('login/', sign_in, name = 'login'),
     path('', TemplateView.as_view(template_name = 'index.html'), name = 'homepage'),
+    path('register/', sign_up, name = 'register'),
+    path('login/', sign_in, name = 'login'),
     path('logout/', sign_out, name = 'logout')
 ]
