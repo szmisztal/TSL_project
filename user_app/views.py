405e9238669748a42c9624cfc0dd545b4572be4d
szmisztal <sz.misztal@gmail.com>
2023-07-17 15:26:09 +0200

    add user registration function

--- user_app/views.py
+++ user_app/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
+
+def sign_up(request):
+    template_name = "register.html"
+    if request.method == "GET":
+        form = RegisterForm()
+        return render(request, template_name, {"form": form})
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit = False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, "Registration success, you can log in now.")
+            return redirect("login")
+        else:
+            return render(request, template_name, {"form": form})
 
 def sign_in(request):
     template_name = "login.html"
@@ -28,5 +44,5 @@ def sign_in(request):
 
 def sign_out(request):
     logout(request)
-    messages.success(request, "You have been log out.")
+    messages.success(request, "You have been logged out.")
     return redirect("login")
