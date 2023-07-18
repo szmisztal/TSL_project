ee54bcbcecc0d4e3eb04e15757cf39cf78b1b7ee
szmisztal <sz.misztal@gmail.com>
2023-07-18 15:35:38 +0200

    add url for homepage

--- TSL_project/urls.py
+++ TSL_project/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from logistician_app import urls as logistician_urls
 from user_app import urls as user_urls
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name = 'index.html'), name = 'homepage'),
     path('users/', include(user_urls.urlpatterns)),
     path('admin/', admin.site.urls),
     path('logistician/', include(logistician_urls.urlpatterns)),
