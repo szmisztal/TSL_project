9dc91ba5acc8122450e5cc8654c88969ed9d760c
szmisztal <sz.misztal@gmail.com>
2023-07-20 11:51:23 +0200

    delete '' url

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from django.views.generic import TemplateView
 from user_app import views
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name = 'index.html'), name = 'homepage'),
     path('register/', views.sign_up, name = 'register'),
     path('login/', views.sign_in, name = 'login'),
     path('logout/', views.sign_out, name = 'logout'),
