b6fccc5c8a4db194e679a5d760364012420919d3
szmisztal <sz.misztal@gmail.com>
2023-07-20 11:50:36 +0200

    delete '' url, and add edit url for assigned orders

--- dispatcher_app/urls.py
+++ dispatcher_app/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from django.views.generic import TemplateView
 from dispatcher_app import views
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name = 'index.html'), name = 'homepage'),
-    path('assign_order/', views.assign_order_to_driver, name = "assign-order"),
-    path('assign_orders_list/', views.AssignedOrdersView.as_view(), name = "assigned-orders-list")
+    path('assign_order/', views.assign_order_to_driver, name = 'assign-order'),
+    path('assign_update/<int:pk>/', views.assign_order_to_driver, name = 'assign-update'),
+    path('assign_orders_list/', views.AssignedOrdersView.as_view(), name = 'assigned-orders-list')
 ]
