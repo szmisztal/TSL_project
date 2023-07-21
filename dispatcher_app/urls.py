3340ccc64f39762a8969eee2d087aa3a226200dd
szmisztal <sz.misztal@gmail.com>
2023-07-21 12:32:15 +0200

    refactor urls

--- dispatcher_app/urls.py
+++ dispatcher_app/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from dispatcher_app import views
 
 urlpatterns = [
-    path('assign_order/', views.assign_order_to_driver, name = 'assign-order'),
-    path('assign_update/<int:pk>/', views.assign_order_to_driver, name = 'assign-update'),
-    path('assign_orders_list/', views.AssignedOrdersView.as_view(), name = 'assigned-orders-list')
+    path('assign_order/', views.OrdersListView.as_view(), name = 'assign-order'),
+     path('make_assign/<int:pk>/', views.assign_order_to_driver, name = 'make-assign'),
 ]
