924bbe59ec9a658a72f18d5e2efa7463e55065ec
szmisztal <sz.misztal@gmail.com>
2023-07-21 12:31:53 +0200

    delete form-field for order

--- dispatcher_app/forms.py
+++ dispatcher_app/forms.py
@@ -2,7 +2,9 @@ from django import forms
 from user_app.models import CustomUser
 from logistician_app.models import TransportationOrder
 
-class AssignForm(forms.Form):
-    driver = forms.ModelChoiceField(queryset = CustomUser.drivers.all(), empty_label = "Select a driver")
-    order = forms.ModelChoiceField(queryset = TransportationOrder.unassigned_orders.all(), empty_label = "Select a order")
+class AssignForm(forms.ModelForm):
+    driver = forms.ModelChoiceField(queryset = CustomUser.drivers.all(), empty_label = "-------", required = False)
 
+    class Meta:
+        model = TransportationOrder
+        fields = ["driver"]
