373599e705db5b4e9ee3f3a00a3468ad815c5152
szmisztal <sz.misztal@gmail.com>
2023-07-21 12:32:50 +0200

    change assign order fucntion

--- dispatcher_app/views.py
+++ dispatcher_app/views.py
@@ -1,41 +1,45 @@
 from django.contrib.auth.decorators import login_required
 from django.db import transaction
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib import messages
 from django.utils.decorators import method_decorator
-from rest_framework.generics import ListAPIView
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from logistician_app.serializers import TransportationOrderSerializer
 from logistician_app.models import TransportationOrder
 from .forms import AssignForm
 
 @login_required
 @transaction.atomic
-def assign_order_to_driver(request):
+def assign_order_to_driver(request, pk):
+    try:
+        order = TransportationOrder.objects.get(pk = pk)
+    except TransportationOrder.DoesNotExist as e:
+        messages.error(request, str(e))
+        return redirect("assign-order")
+
     if request.method == "POST":
         form = AssignForm(request.POST)
         if form.is_valid():
             driver = form.cleaned_data["driver"]
-            order = form.cleaned_data["order"]
-            try:
-                order.driver = driver
-                order.save()
-                return redirect("assign-orders-list")
-            except Exception as e:
-                messages.error(request, str(e))
+            order.driver = driver
+            order.save()
+            messages.success(request, "Assign done.")
+            return redirect("assign-order")
     else:
         form = AssignForm()
-    return render(request, "assign_order_to_driver.html", {"form": form})
+
+    return render(request, "assign_order_form.html", {"form": form, "order": order})
 
 @method_decorator(login_required, name = "dispatch")
-class AssignedOrdersView(ListAPIView):
+class OrdersListView(APIView):
     serializer_class = TransportationOrderSerializer
     renderer_classes = [TemplateHTMLRenderer]
-    template_name = "assigned_orders_list.html"
+    template_name = "assign_order.html"
 
     def get(self, request, *args, **kwargs):
-        orders = TransportationOrder.objects.all().order_by("driver")
+        orders = TransportationOrder.objects.all().order_by("date")
         return Response({"serializer": self.serializer_class(orders), "orders": orders},
                         template_name = self.template_name)
 
