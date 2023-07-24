835ca72aba5d41ce9ed20e1a61231253b7a4fe3c
szmisztal <sz.misztal@gmail.com>
2023-07-24 17:22:06 +0200

    change homepage url. templateview ====> homepage function

--- TSL_project/urls.py
+++ TSL_project/urls.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
+from user_app import urls as user_urls
 from logistician_app import urls as logistician_urls
 from dispatcher_app import urls as dispatcher_urls
-from user_app import urls as user_urls
+from driver_app import urls as driver_urls
+from .views import HomepageView
+
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name = 'index.html'), name = 'homepage'),
+    path('', HomepageView.as_view(), name = "homepage"),
     path('admin/', admin.site.urls),
     path('users/', include(user_urls.urlpatterns)),
     path('logistician/', include(logistician_urls.urlpatterns)),
-    path('dispatcher/', include(dispatcher_urls.urlpatterns))
+    path('dispatcher/', include(dispatcher_urls.urlpatterns)),
+    path('driver/', include(driver_urls.urlpatterns))
 ]
