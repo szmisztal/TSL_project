154e833a389693b32702d18c5d395972f514f4b9
szmisztal <sz.misztal@gmail.com>
2023-07-25 14:39:53 +0200

    add backend options for sending emails

--- TSL_project/settings.py
+++ TSL_project/settings.py
@@ -107,3 +107,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
+EMAIL_HOST_USER = 'twoj.adres@gmail.com'
+EMAIL_HOST_PASSWORD = 'twojehaslo'
