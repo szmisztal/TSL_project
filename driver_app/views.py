044412cbd0ed9e064fcdea7ce3fe99b09a0381f5
szmisztal <sz.misztal@gmail.com>
2023-07-25 14:40:46 +0200

    add send emails function, after order will be finish

--- driver_app/views.py
+++ driver_app/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.mail import send_mail
 from rest_framework.decorators import permission_classes
 from user_app.permissions import IsDriver
+from user_app.models import CustomUser
 from logistician_app.models import TransportationOrder
 from .forms import OrderDoneForm
 
@@ -23,7 +25,27 @@ def current_order(request):
             order.driver = None
             order.save()
             messages.success(request, "Order finished")
+            send_emails(request)
             return redirect("homepage")
     else:
         form = OrderDoneForm()
     return render(request, "driver_order_form.html", {"form": form, "order": order})
+
+def send_emails(request):
+    emails = []
+    driver = request.user
+    dispatchers = CustomUser.objects.filter(role = "Dispatcher")
+    for user in dispatchers:
+        email = user.email
+        emails.append(email)
+
+    send_mail(
+        "ORDER FINISHED",
+        f"{driver.first_name} {driver.last_name} finished his order.",
+        "app.mail@gmail.com",
+        emails + ["sz.misztal@gmail.com"],
+        fail_silently=False
+    )
+
+
+
