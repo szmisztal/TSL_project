ed3da580499ded37585879f769df1dc670b61596
szmisztal <sz.misztal@gmail.com>
2023-07-31 12:39:21 +0200

    refactor urls

--- logistician_app/urls.py
+++ logistician_app/urls.py
@@ -4,18 +4,18 @@ from logistician_app import views
 urlpatterns = [
     path('orders_list/', views.TransportationOrderView.as_view(), name = 'orders-list'),
     path('order/<int:pk>/', views.TransportationOrderView.as_view(), name = 'order-retrieve'),
-    path('order_create/', views.TransportationOrderCreateView.as_view(), name = 'order-create'),
-    path('order_update/<int:pk>/', views.TransportationOrderUpdateView.as_view(), name = 'order-update'),
+    path('order_create/', views.TransportationOrderCreateOrUpdateView.as_view(), name = 'order-create'),
+    path('order_update/<int:pk>/', views.TransportationOrderCreateOrUpdateView.as_view(), name = 'order-update'),
     path('order_destroy/<int:pk>/', views.TransportationOrderDestroyView.as_view(), name = 'order-destroy'),
 
-    path('places_list/', views.PlaceListView.as_view(), name = 'places-list'),
-    path('place/<int:pk>/', views.PlaceRetrieveView.as_view(), name = 'place-retrieve'),
-    path('place_create/', views.PlaceCreateView.as_view(), name = 'place-create'),
-    path('place_update/<int:pk>/', views.PlaceUpdateView.as_view(), name = 'place-update'),
+    path('places_list/', views.PlaceView.as_view(), name = 'places-list'),
+    path('place/<int:pk>/', views.PlaceView.as_view(), name = 'place-retrieve'),
+    path('place_create/', views.PlaceCreateOrUpdateView.as_view(), name = 'place-create'),
+    path('place_update/<int:pk>/', views.PlaceCreateOrUpdateView.as_view(), name = 'place-update'),
     path('place_destroy/<int:pk>/', views.PlaceDestroyView.as_view(), name = 'place-destroy'),
 
-    path('tanker_create/', views.TankerCreateView.as_view(), name = 'tanker-create'),
-    path('tanker_update/<int:pk>/', views.TankerUpdateView.as_view(), name = 'tanker-update'),
+    path('tanker_create/', views.TankerCreateOrUpdateView.as_view(), name = 'tanker-create'),
+    path('tanker_update/<int:pk>/', views.TankerCreateOrUpdateView.as_view(), name = 'tanker-update'),
 ]
 
 
