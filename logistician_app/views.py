c9a27d1787f2d2d655a0f616799dabbe594dfc88
szmisztal <sz.misztal@gmail.com>
2023-07-31 12:40:00 +0200

    refactor views. i repeat to much code so decide to refactor all views

--- logistician_app/views.py
+++ logistician_app/views.py
@@ -3,9 +3,7 @@ from django.utils.decorators import method_decorator
 from django.db import transaction
 from django.shortcuts import get_object_or_404, redirect
 from django.contrib import messages
-from django.core.exceptions import ValidationError
 from rest_framework import status
-from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
 from rest_framework.decorators import permission_classes
@@ -34,100 +32,82 @@ class TransportationOrderView(APIView):
 
 @method_decorator(login_required, name = "dispatch")
 @permission_classes([IsLogistician])
-class TransportationOrderCreateView(CreateAPIView):
-    form_class = OrderForm
-    renderer_classes = [TemplateHTMLRenderer]
-    template_name = "order_form.html"
-
-    def get(self, request):
-        form = self.form_class()
-        return Response({"form": form}, template_name = self.template_name)
-
-    @transaction.atomic
-    def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            order = form.save(commit = False)
-            load_place_id = order.load_place_id
-            delivery_place_id = order.delivery_place_id
-            try:
-                order.place_restrict(load_place_id = load_place_id, delivery_place_id = delivery_place_id)
-                order.empty_tanker_volume()
-            except ValueError as e:
-                messages.error(self.request, str(e))
-                form.initial = form.cleaned_data
-                return Response({"form": form}, template_name = self.template_name, status = status.HTTP_400_BAD_REQUEST)
-            try:
-                order.full_clean()
-            except ValidationError as e:
-                messages.error(self.request, str(e))
-                form.initial = form.cleaned_data
-                return Response({"form": form}, template_name = self.template_name, status = status.HTTP_400_BAD_REQUEST)
-            order.save()
-            messages.success(self.request, "Transportation order created successfully.")
-            return redirect("orders-list")
-        else:
-            return Response({"form": form}, template_name = self.template_name, status = status.HTTP_400_BAD_REQUEST)
-
-@method_decorator(login_required, name = "dispatch")
-@permission_classes([IsLogistician])
-class TransportationOrderUpdateView(UpdateAPIView):
-    queryset = TransportationOrder.objects.all()
+class TransportationOrderCreateOrUpdateView(APIView):
     serializer_class = TransportationOrderSerializer
     form_class = OrderForm
     renderer_classes = [TemplateHTMLRenderer]
     template_name = "order_form.html"
 
     def get(self, request, *args, **kwargs):
-        order = self.get_object()
-        tanker = order.tanker_volume
-        form = self.form_class(instance = order)
-        return Response({"serializer": self.serializer_class(order), "form": form, "order": order, "tanker": tanker},
-                        template_name = self.template_name)
+        order_id = kwargs.get("pk")
+        if order_id:
+            order = get_object_or_404(TransportationOrder, id = order_id)
+            tanker = order.tanker_volume
+            form = self.form_class(instance = order)
+            return Response({"serializer": self.serializer_class(order), "form": form, "order": order, "tanker": tanker},
+                            template_name = self.template_name)
+        else:
+            form = self.form_class()
+            return Response({"form": form}, template_name = self.template_name)
+
+    def validate_and_save_order(self, order, form):
+        try:
+            order.place_restrict()
+            order.empty_tanker_volume()
+        except Exception as e:
+            messages.error(self.request, str(e))
+            form.initial = form.cleaned_data
+            return Response({"form": form}, template_name = self.template_name, status = status.HTTP_400_BAD_REQUEST)
+        try:
+            order.full_clean()
+        except Exception as e:
+            messages.error(self.request, str(e))
+            form.initial = form.cleaned_data
+            return Response({"form": form}, template_name = self.template_name, status = status.HTTP_400_BAD_REQUEST)
+        order.save()
+        return None
 
     @transaction.atomic
     def post(self, request, *args, **kwargs):
-        order = self.get_object()
-        tanker = order.tanker_volume
-        form = self.form_class(request.POST, instance = order)
-        if form.is_valid():
-            order = form.save(commit = False)
-            load_place_id = order.load_place_id
-            delivery_place_id = order.delivery_place_id
-            try:
-                order.place_restrict(load_place_id = load_place_id, delivery_place_id = delivery_place_id)
-                order.empty_tanker_volume()
-            except ValueError as e:
-                messages.error(self.request, str(e))
-                form.initial = form.cleaned_data
-                return Response({"form": form}, template_name = self.template_name, status = status.HTTP_400_BAD_REQUEST)
-            try:
-                order.full_clean()
-            except ValidationError as e:
-                messages.error(self.request, str(e))
-                form.initial = form.cleaned_data
-                return Response({"form": form}, template_name = self.template_name, status = status.HTTP_400_BAD_REQUEST)
-            order.save()
-            messages.success(self.request, "Transportation order updated successfully.")
-            return redirect("orders-list")
+        order_id = kwargs.get("pk")
+        form = self.form_class(request.POST)
+        if order_id:
+            order = get_object_or_404(TransportationOrder, id = order_id)
+            form = self.form_class(request.POST, instance = order)
+            tanker = order.tanker_volume
+            if form.is_valid():
+                order = form.save(commit = False)
+                response = self.validate_and_save_order(order, form)
+                if response is not None:
+                    return response
+                messages.success(self.request, "Transportation order updated successfully.")
+                return redirect("orders-list")
         else:
-            return Response({"form": form, "tanker": tanker}, template_name = self.template_name, status = status.HTTP_400_BAD_REQUEST)
+            if form.is_valid():
+                order = form.save(commit = False)
+                response = self.validate_and_save_order(order, form)
+                if response is not None:
+                    return response
+                messages.success(self.request, "Transportation order created successfully.")
+                return redirect("orders-list")
+        return Response({"form": form, "tanker": tanker}, template_name = self.template_name, status = status.HTTP_400_BAD_REQUEST)
 
 @method_decorator(login_required, name = "dispatch")
 @permission_classes([IsLogistician])
-class TransportationOrderDestroyView(DestroyAPIView):
-    queryset = TransportationOrder.objects.all()
+class TransportationOrderDestroyView(APIView):
     serializer_class = TransportationOrderSerializer
     renderer_classes = [TemplateHTMLRenderer]
     template_name = "order_delete.html"
 
     def get(self, request, *args, **kwargs):
-        order = self.get_object()
+        order_id = kwargs.get("pk")
+        order = get_object_or_404(TransportationOrder, id = order_id)
         return Response({"serializer": self.serializer_class(order), "order": order},
                         template_name = self.template_name)
 
     def post(self, request, *args, **kwargs):
-        order = self.get_object()
+        order_id = kwargs.get("pk")
+        order = get_object_or_404(TransportationOrder, id = order_id)
         tanker = order.tanker_volume
         order.delete()
         if tanker:
@@ -137,22 +117,73 @@ class TransportationOrderDestroyView(DestroyAPIView):
 
 @method_decorator(login_required, name = "dispatch")
 @permission_classes([IsLogistician])
-class PlaceListView(ListAPIView):
+class PlaceView(APIView):
     serializer_class = LoadOrDeliveryPlaceSerializer
     renderer_classes = [TemplateHTMLRenderer]
-    template_name = "places_list.html"
 
     def get(self, request, *args, **kwargs):
         places = LoadOrDeliveryPlace.objects.all().order_by("country")
-        return Response({"serializer": self.serializer_class(places), "places": places},
-                        template_name = self.template_name)
+        place_id = kwargs.get("pk")
+        if place_id:
+            place = get_object_or_404(LoadOrDeliveryPlace, id = place_id)
+            return Response({"serializer": self.serializer_class(place), "place": place},
+                        template_name = "place_retrieve.html")
+        else:
+            return Response({"serializer": self.serializer_class(places), "places": places},
+                        template_name = "places_list.html")
 
 @method_decorator(login_required, name = "dispatch")
 @permission_classes([IsLogistician])
-class PlaceRetrieveView(RetrieveAPIView):
+class PlaceCreateOrUpdateView(APIView):
+    serializer_class = LoadOrDeliveryPlaceSerializer
+    form_class = PlaceForm
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = "place_form.html"
+
+    def get(self, request, *args, **kwargs):
+        place_id = kwargs.get("pk")
+        if place_id:
+            place = get_object_or_404(LoadOrDeliveryPlace, id = place_id)
+            form = self.form_class(instance = place)
+            return Response({"serializer": self.serializer_class(place), "form": form, "place": place},
+                        template_name = self.template_name)
+        else:
+            form = self.form_class()
+            return Response({"form": form}, template_name = self.template_name)
+
+    def save_place(self, form, place = None):
+        if form.is_valid():
+            if place is None:
+                place = form.save()
+                success_message = "Place created successfully."
+            else:
+                place = form.save()
+                success_message = "Place updated successfully."
+            messages.success(self.request, success_message)
+            return place
+        return None
+
+    @transaction.atomic
+    def post(self, request, *args, **kwargs):
+        place_id = kwargs.get("pk")
+        form = self.form_class(request.POST)
+        if place_id:
+            place = get_object_or_404(LoadOrDeliveryPlace, id = place_id)
+            form = self.form_class(request.POST, instance = place)
+        else:
+            place = None
+        saved_place = self.save_place(form, place)
+        if saved_place:
+            return redirect("places-list")
+        else:
+            return Response({"form": form}, template_name = self.template_name, status = status.HTTP_400_BAD_REQUEST)
+
+@method_decorator(login_required, name = "dispatch")
+@permission_classes([IsLogistician])
+class PlaceDestroyView(APIView):
     serializer_class = LoadOrDeliveryPlaceSerializer
     renderer_classes = [TemplateHTMLRenderer]
-    template_name = "place_retrieve.html"
+    template_name = "place_delete.html"
 
     def get(self, request, *args, **kwargs):
         place_id = kwargs.get("pk")
@@ -160,116 +191,54 @@ class PlaceRetrieveView(RetrieveAPIView):
         return Response({"serializer": self.serializer_class(place), "place": place},
                         template_name = self.template_name)
 
-@method_decorator(login_required, name = "dispatch")
-@permission_classes([IsLogistician])
-class PlaceCreateView(CreateAPIView):
-    form_class = PlaceForm
-    renderer_classes = [TemplateHTMLRenderer]
-    template_name = "place_form.html"
-
-    def get(self, request):
-        form = self.form_class()
-        return Response({"form": form}, template_name = self.template_name)
-
-    @transaction.atomic
     def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(self.request, "Place created successfully.")
+        place_id = kwargs.get("pk")
+        place = get_object_or_404(LoadOrDeliveryPlace, id = place_id)
+        try:
+            place.delete()
+            messages.success(self.request, "Place deleted successfully.")
             return redirect("places-list")
-        else:
-            return Response({"form": form}, template_name = self.template_name, status = status.HTTP_400_BAD_REQUEST)
-
-@method_decorator(login_required, name = "dispatch")
-@permission_classes([IsLogistician])
-class PlaceUpdateView(UpdateAPIView):
-    queryset = LoadOrDeliveryPlace.objects.all()
-    serializer_class = LoadOrDeliveryPlaceSerializer
-    form_class = PlaceForm
-    renderer_classes = [TemplateHTMLRenderer]
-    template_name = "place_form.html"
-
-    def get(self, request, *args, **kwargs):
-        place = self.get_object()
-        form = self.form_class(instance = place)
-        return Response({"serializer": self.serializer_class(place), "form": form, "place": place},
-                        template_name = self.template_name)
-
-    @transaction.atomic
-    def post(self, request, *args, **kwargs):
-        place = self.get_object()
-        form = self.form_class(request.POST, instance = place)
-        if form.is_valid():
-            form.save()
-            messages.success(self.request, "Place updated successfully.")
+        except Exception as e:
+            messages.error(self.request, "This place belongs to some transportation order.")
             return redirect("places-list")
-        else:
-            return Response({"form": form}, template_name = self.template_name, status = status.HTTP_400_BAD_REQUEST)
 
 @method_decorator(login_required, name = "dispatch")
 @permission_classes([IsLogistician])
-class PlaceDestroyView(DestroyAPIView):
-    queryset = LoadOrDeliveryPlace.objects.all()
-    serializer_class = LoadOrDeliveryPlaceSerializer
-    renderer_classes = [TemplateHTMLRenderer]
-    template_name = "place_delete.html"
-
-    def get(self, request, *args, **kwargs):
-        place = self.get_object()
-        return Response({"serializer": self.serializer_class(place), "place": place},
-                        template_name = self.template_name)
-
-    def post(self, request, *args, **kwargs):
-        place = self.get_object()
-        place.delete()
-        messages.success(self.request, "Place deleted successfully.")
-        return redirect("places-list")
-
-@method_decorator(login_required, name = "dispatch")
-@permission_classes([IsLogistician])
-class TankerCreateView(CreateAPIView):
-    form_class = TankerForm
-    renderer_classes = [TemplateHTMLRenderer]
-    template_name = "tanker_form.html"
-
-    def get(self, request):
-        form = self.form_class()
-        return Response({"form": form}, template_name = self.template_name)
-
-    @transaction.atomic
-    def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(self.request, "Tanker trailer volumes set up successfully.")
-            return redirect("order-create")
-        else:
-            return Response({"form": form}, template_name = self.template_name, status = status.HTTP_400_BAD_REQUEST)
-
-@method_decorator(login_required, name = "dispatch")
-@permission_classes([IsLogistician])
-class TankerUpdateView(UpdateAPIView):
-    queryset = TankerTrailer.objects.all()
+class TankerCreateOrUpdateView(APIView):
     serializer_class = TankerTrailerSerializer
     form_class = TankerForm
     renderer_classes = [TemplateHTMLRenderer]
     template_name = "tanker_form.html"
 
     def get(self, request, *args, **kwargs):
-        tanker = self.get_object()
-        form = self.form_class(instance = tanker)
-        return Response({"serializer": self.serializer_class(tanker), "form": form, "tanker": tanker},
+        tanker_id = kwargs.get("pk")
+        if tanker_id:
+            tanker = get_object_or_404(TankerTrailer, id = tanker_id)
+            form = self.form_class(instance = tanker)
+            return Response({"serializer": self.serializer_class(tanker), "form": form, "tanker": tanker},
                         template_name = self.template_name)
+        else:
+            form = self.form_class()
+            return Response({"form": form}, template_name = self.template_name)
 
     @transaction.atomic
     def post(self, request, *args, **kwargs):
-        tanker = self.get_object()
-        form = self.form_class(request.POST, instance = tanker)
-        if form.is_valid():
-            form.save()
-            messages.success(self.request, "Tanker trailer volumes updated successfully.")
-            return redirect("orders-list")
+        tanker_id = kwargs.get("pk")
+        if tanker_id:
+            tanker = get_object_or_404(TankerTrailer, id = tanker_id)
+            form = self.form_class(request.POST, instance = tanker)
+            if form.is_valid():
+                form.save()
+                messages.success(self.request, "Tanker trailer volumes updated successfully.")
+                return redirect("orders-list")
+            else:
+                return Response({"form": form}, template_name = self.template_name, status = status.HTTP_400_BAD_REQUEST)
         else:
-            return Response({"form": form}, template_name = self.template_name, status = status.HTTP_400_BAD_REQUEST)
+            form = self.form_class(request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(self.request, "Tanker trailer volumes set up successfully.")
+                return redirect("order-create")
+            else:
+                return Response({"form": form}, template_name = self.template_name, status = status.HTTP_400_BAD_REQUEST)
 
