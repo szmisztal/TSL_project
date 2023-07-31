112fb8ad269d92939aec0843a35a20b31195a36a
szmisztal <sz.misztal@gmail.com>
2023-07-31 10:29:14 +0200

    change url for retrieve/list order views

--- logistician_app/urls.py
+++ logistician_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from logistician_app import views
 
 urlpatterns = [
-    path('orders_list/', views.TransportationOrderListView.as_view(), name = 'orders-list'),
-    path('order/<int:pk>/', views.TransportationOrderRetrieveView.as_view(), name = 'order-retrieve'),
+    path('orders_list/', views.TransportationOrderView.as_view(), name = 'orders-list'),
+    path('order/<int:pk>/', views.TransportationOrderView.as_view(), name = 'order-retrieve'),
     path('order_create/', views.TransportationOrderCreateView.as_view(), name = 'order-create'),
     path('order_update/<int:pk>/', views.TransportationOrderUpdateView.as_view(), name = 'order-update'),
     path('order_destroy/<int:pk>/', views.TransportationOrderDestroyView.as_view(), name = 'order-destroy'),
@@ -17,7 +17,6 @@ urlpatterns = [
     path('tanker_create/', views.TankerCreateView.as_view(), name = 'tanker-create'),
     path('tanker_update/<int:pk>/', views.TankerUpdateView.as_view(), name = 'tanker-update'),
 ]
-
 
 
 
