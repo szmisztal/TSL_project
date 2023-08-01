5a0f291b1e3fc88af5130bc0f04d870760502a7e
szmisztal <sz.misztal@gmail.com>
2023-08-01 12:10:37 +0200

    fix email_backend options

--- TSL_project/settings.py
+++ TSL_project/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from .password import PASSWORD
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -109,9 +110,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_HOST_USER = 'twoj.adres@gmail.com'
-EMAIL_HOST_PASSWORD = 'twojehaslo'
+EMAIL_HOST_USER = 'sz.misztal@gmail.com'
+EMAIL_HOST_PASSWORD = PASSWORD
+DEFAULT_FROM_EMAIL = 'sz.misztal@gmail.com'
+
