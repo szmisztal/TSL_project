6e051cc19163d6118badac1e985b7197ae5283d1
szmisztal <sz.misztal@gmail.com>
2023-08-04 17:01:27 +0200

    fix BooleanField in form - add '()' on the end

--- driver_app/forms.py
+++ driver_app/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from logistician_app.models import TransportationOrder
 
 class OrderDoneForm(forms.ModelForm):
-    done = forms.BooleanField
+    done = forms.BooleanField()
 
     class Meta:
         model = TransportationOrder
