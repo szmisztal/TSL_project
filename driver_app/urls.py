a672b9533f8569a182a1034e5e8b59c01d146fa0
szmisztal <sz.misztal@gmail.com>
2023-08-06 14:06:18 +0200

    change view to CurrentOrder.as_view() from current_order.as_view()

--- driver_app/urls.py
+++ driver_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from driver_app import views
 
 urlpatterns = [
-    path('your_order/', views.current_order, name = 'current-order')
+    path('your_order/', views.CurrentOrder.as_view(), name = 'current-order')
 ]
