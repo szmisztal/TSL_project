a1012a60694b59844af403d9ef37e028f332926e
szmisztal <sz.misztal@gmail.com>
2023-08-09 16:13:34 +0200

    delete unnecessary space

--- dispatcher_app/urls.py
+++ dispatcher_app/urls.py
@@ -3,5 +3,5 @@ from dispatcher_app import views
 
 urlpatterns = [
     path('assign_order/', views.OrdersListView.as_view(), name = 'assign-order'),
-     path('make_assign/<int:pk>/', views.assign_order_to_driver, name = 'make-assign'),
+    path('make_assign/<int:pk>/', views.assign_order_to_driver, name = 'make-assign'),
 ]
